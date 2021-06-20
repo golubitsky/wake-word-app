744359ae4e259ec8c919b56e00c7503a142d7664
Mikhail Golubitsky <mike617@gmail.com>
2021-06-19 20:20:01 -0400

    bump

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, brave world! Revision 2.'
+    return 'Hello, brave world! Revision 3.'
 
 
 if __name__ == '__main__':
